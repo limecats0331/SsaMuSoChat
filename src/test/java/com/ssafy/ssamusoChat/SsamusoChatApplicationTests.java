cb11b93b16831fb2f7c9f055cbba0c1e701db74c
이지용 <dlwldyd9303@naver.com>
2022-11-10 13:15:09 +0900

    feat(BE): 채팅 서버 프로젝트 생성

---
+++ src/test/java/com/ssafy/ssamusoChat/SsamusoChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssamusoChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsamusoChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
